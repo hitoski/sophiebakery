64dff8b89f9acee80122e3a2bf13355cd4731ed1
hiTOski <albinguillaume59@gmail.com>
2016-09-23 04:57:37 -0700

    c'est le premier commit de la vie git add .!

---
+++ README.md
@@ -0,0 +1,4 @@
+sophierecepies
+==============
+
+A Symfony project created on September 22, 2016, 1:05 pm.
---
+++ src/SophieBundle/SophieBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SophieBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SophieBundle extends Bundle
+{
+}
